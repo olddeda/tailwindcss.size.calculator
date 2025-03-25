220628b9b79023b73e7784bfb1517c92c1df50f3
safronov <safronov.ser@icloud.com>
2025-03-25 16:45:07 +0300

    Add files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   
   app: {
-    baseURL: '/tailwindcss.size.calculator/docs/'
+    baseURL: '/tailwindcss.size.calculator/'
   },
 
   modules: [
